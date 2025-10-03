be8ecc5e4230b714d97b3692482fd37476dda691
Nandhakumar K <116391685+Tamilupdates@users.noreply.github.com>
2025-10-04 00:04:50 +0530

    latest

--- KPS/utils/render_template.py
+++ KPS/utils/render_template.py
@@ -33,7 +33,7 @@ async def render_page(id: int, secure_hash: str, requested_action: str | None = 
             raise InvalidHash("File unique ID or secure hash mismatch during rendering.")
         
         quoted_filename = urllib.parse.quote(file_name.replace('/', '_'))
-        src = urllib.parse.urljoin(Var.URL, f'{secure_hash}{id}}')
+        src = urllib.parse.urljoin(Var.URL, f'{secure_hash}{id}')
         safe_filename = html_module.escape(file_name)
         if requested_action == 'stream':
             template = template_env.get_template('req.html')
